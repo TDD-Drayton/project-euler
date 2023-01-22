e2beb8d07a15053e09192e17f238a0970545a020
Toi Drayton <47548569+TDD-Drayton@users.noreply.github.com>
2023-01-21 21:30:18 -0500

    Updates to 5 and 6
    5 and 6 solved without the use of a library

---
+++ 1-10 Solutions/euler05.py
@@ -0,0 +1,15 @@
+#What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+import math
+
+#returns least common multiple of a and b
+#helper function
+def lcm(a,b):
+    return a*b // math.gcd(a, b)
+
+#Uses helper function to iteratively find the least common multiple of numbers 1 to 'n'
+def smallest_multiple(n):
+    result = 1
+    for i in range(1, n+1):
+        result = lcm(result, i)
+    return result
+#
